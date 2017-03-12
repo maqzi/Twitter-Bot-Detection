d307b4ec378eda06996e690f5e13d71175ade1dd
Munaf A Qazi <munaf.qazi@gmail.com>
2017-03-11 20:46:40 -0500

    saving user response objects into a csv

---
+++ twitter-api-request.py
@@ -0,0 +1,94 @@
+
+import requests, os, csv, pandas
+from requests_oauthlib import OAuth1
+from urllib.parse import parse_qs
+
+
+REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
+AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize?oauth_token="
+ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
+
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+
+OAUTH_TOKEN = ""
+OAUTH_TOKEN_SECRET = ""
+
+REQUIRED_FIELDS = ['id','id_str','screen_name','location','description','url',
+                   'followers_count','friends_count', 'listed_count', 'created_at',
+                   'favorites_count', 'verified','statuses_count','lang','status',
+                   'default_profile','default_profile_image','has_extended_profile',
+                   'name','bot']
+
+def setup_oauth():
+    # Request token
+    oauth = OAuth1(CONSUMER_KEY, client_secret=CONSUMER_SECRET)
+    r = requests.post(url=REQUEST_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+
+    resource_owner_key = credentials.get('oauth_token')[0]
+    resource_owner_secret = credentials.get('oauth_token_secret')[0]
+
+    # Authorize
+    authorize_url = AUTHORIZE_URL + resource_owner_key
+    print ('Please go here and authorize: ' + authorize_url)
+
+    verifier = input('Please input the verifier: ')
+    oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+                   resource_owner_key=resource_owner_key,
+                   resource_owner_secret=resource_owner_secret,
+                   verifier=verifier)
+
+    # Finally, Obtain the Access Token
+    r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+    token = credentials.get('oauth_token')[0]
+    secret = credentials.get('oauth_token_secret')[0]
+
+    return token, secret
+
+
+def get_oauth():
+    oauth = OAuth1(CONSUMER_KEY,
+                client_secret=CONSUMER_SECRET,
+                resource_owner_key=OAUTH_TOKEN,
+                resource_owner_secret=OAUTH_TOKEN_SECRET)
+    return oauth
+
+def write_to_csv(responses):
+    user_l = []
+    for response in responses.values():
+        final_fields = {}
+        response_dict = response.json()[0]
+        for key, value in response_dict.items():
+            if key in REQUIRED_FIELDS:
+                final_fields[key] = value
+        user_l.append(final_fields)
+
+        user_df = pandas.DataFrame(user_l)
+    print(user_df)
+
+    twitter_user_data = open(os.getcwd() + '/twitterUsers.csv', 'w')
+    user_df.to_csv(twitter_user_data,index=False)
+    twitter_user_data.close()
+
+if __name__ == "__main__":
+    if not OAUTH_TOKEN:
+        token, secret = setup_oauth()
+        print ("OAUTH_TOKEN: " + token)
+        print ("OAUTH_TOKEN_SECRET: " + secret)
+    else:
+        oauth = get_oauth()
+        screen_name = ['justinbieber','potus']
+        responses = {}
+        for name in screen_name:
+            responses[name] = requests.get(url="https://api.twitter.com/1.1/users/lookup.json?screen_name="+str(name), auth=oauth)
+        write_to_csv(responses)
+
+#        r = requests.get(url="https://api.twitter.com/1.1/statuses/mentions_timeline.json", auth=oauth)
+#        print (r.json())
+
+
+
+
